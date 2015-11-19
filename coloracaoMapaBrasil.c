7ca4d11d8d14a2874400368decb0c8d0e123c97c
daniel <silva10.daniel@gmail.com>
2015-11-19 18:02:50 -0200

    versao final algoritmo

--- coloracaoMapaBrasil.c
+++ coloracaoMapaBrasil.c
@@ -30,10 +30,11 @@ char *estados[26][12]= {{"BA", "SE", "AL","PE","PI","MA","TO","GO","MG","ES", "e
 { "SE","AL","BA", "end"},
 { "RS","SC", "end"},
 { "AP","PA", "end"}};
+char *cor [4];
 
 struct vertice {
     char *sigla;
-
+    int cor;//indice da cor no vetor de cores
     struct vertice *prox;
 };
 
@@ -43,10 +44,54 @@ struct listaadj{
 
 struct listaadj *Adj;
 struct vertice *novo;
-int *marcado;
+
+
+//encontra um cor para o primeiro vertice da lista de vertice.
+//retorna 1 se a cor nao estiver sendo usada no vizinho.
+//retorna 0 caso contrario
+int encontrarCor (struct vertice *listaVertice, int cor){
+    int linha;
+
+    struct vertice *v;
+    v = listaVertice->prox;
+    while(v != NULL){
+        for (linha=0; linha < 26; linha++){//percorre o grafo
+            if (strcmp(v->sigla, Adj[linha].listav->sigla ) == 0){//encontra o vizinho no grafo
+               if (cor == Adj[linha].listav->cor){//cor selecionada ja esta sendo usada no vizinho do vertice
+                    return 0;//false
+               }else{
+                    break;
+               }
+            }
+        }
+        v = v->prox;
+    }
+    return 1;
+
+
+}
+
+void  colorirMapaBrasil(char cor1[], char cor2[], char cor3[], char cor4[]){
+    cor[0] = cor1;
+    cor[1] = cor2;
+    cor[2] = cor3;
+    cor[3] = cor4;
+    int linha, indiceCor;
+    //percorre o grafo
+    for (linha=0; linha < 26; linha++){
+        indiceCor = 0;
+        do{//percorre as cores
+            if (encontrarCor(Adj[linha].listav, indiceCor) == 1){
+                Adj[linha].listav->cor = indiceCor;//colore o vertice
+                break;//encontrou cor e coloriu
+            }
+            indiceCor++;
+         }while (indiceCor < 4);
+    }
+}
+
 
 void inserirEstadosNoGrafo(){
-
     int  linha, coluna;
     struct vertice *v, fim;
 
@@ -56,43 +101,39 @@ void inserirEstadosNoGrafo(){
                novo = (struct vertice*) malloc(sizeof(struct vertice));
                novo->sigla = estados[linha][coluna];
                novo->prox = NULL;
-               printf("criou novo\n");
+
                //encontra o fim da lista de vertices e insere a sigla no fim dela
                v = Adj[linha].listav;
                if (v == NULL){
                   Adj[linha].listav = novo;
-                  printf("inseriu no v = null\n");
+
                 }else{
                    while(v->prox != NULL){
                      v = v->prox;
                      //printf("dentro while\n");
                    }
                    v->prox = novo;
-                   printf("inseriu novo\n");
+
                 }
-               //link p/ lista
           }
-          printf("inseriu uma estado e seus vizinhos\n");
        }
 
 
 }
-//aloca memoria para o grafo e para o vetor de itens visitados
+//aloca memoria para o grafo
 void criarGrafo(){
     int i;
 
     Adj = (struct listaadj*) malloc(sizeof(struct listaadj)*(TAM+1));
-    marcado = (int*) malloc(sizeof (int) * (TAM+1));
 
 
     for (i=1; i <=TAM; i++){
         Adj[i].listav = NULL;
-        marcado[i] = 0;
     }
 }
-
+//imprime o grafo sem a coloracao
 void imprimirGrafo(){
-    printf(" \n\nLista adjacencia\n\n");
+    printf("Lista adjacencia\n\n");
     struct vertice *v;
     int i;
     for(i=0; i < TAM; i++){
@@ -107,6 +148,18 @@ void imprimirGrafo(){
     printf("\n\n");
 }
 
+//imprime a lita de adjacencia com os vertices coloridos
+void imprimirGrafoColorido(){
+    printf(" \n\nGrafo Colorido\n\n");
+    struct vertice *v;
+    int i;
+    for(i=0; i < TAM; i++){
+        printf ("%s -> %s \n", Adj[i].listav->sigla, cor[Adj[i].listav->cor]);
+    }
+    printf("\n\n");
+}
+
+//imprime os estados que serao colocados no grafo
 void imprimirEstados(){
     int i, linha, coluna;
 
@@ -117,16 +170,29 @@ void imprimirEstados(){
           }
           printf("\n");
        }
+}
 
-
+//desaloca a lista de adjacencia
+void desalocarGrafo(){
+    struct vertice *aux;
+    int i;
+    for(i=0; i < TAM; i++){
+        aux = Adj[i].listav;
+        Adj[i].listav = Adj[i].listav->prox;
+        while (Adj[i].listav !=NULL){
+            free(aux);
+            aux = Adj[i].listav;
+            Adj[i].listav = Adj[i].listav->prox;
+        }
+    }
+    free(Adj);
 }
 
 int main (){
-    //imprimirEstados();
     criarGrafo();
     inserirEstadosNoGrafo();
     imprimirGrafo();
-
+    colorirMapaBrasil("blue", "red", "yellow", "green");
+    imprimirGrafoColorido();
+    desalocarGrafo();
 }
-
-
